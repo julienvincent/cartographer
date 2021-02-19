adfb9431b142875206f02818d2a4cb0596deb3d7
nicholaswrobinson <n21robin@gmail.com>
2021-02-19 23:24:22 +0200

    feature: added map generator code

---
+++ mapgen/src/blockgen.ts
@@ -0,0 +1,31 @@
+import * as pixels from '@cartographer/pixels';
+import { MCBlockVariant } from '@cartographer/pixels/dist/defs';
+import { generateOptimizedStaircase } from './staircase';
+
+export const buildBlockSpace = (mcBlockGrid: pixels.defs.MCBlockGrid): MCBlockSpace => {
+  const space: MCBlockSpace = [];
+  const width: number = mcBlockGrid[0].length;
+  const height: number = mcBlockGrid.length;
+
+  for (let x = 0; x < width; x++) {
+    const strip: MCBlockVariant[] = mcBlockGrid.map((a) => a[x]); // to redo
+    const staircase: number[] = generateOptimizedStaircase(strip, (s) => s.height_variant);
+
+    space[x] = [defaultNorthBorderBlock].concat(strip).map((v, y) => [{ block_id: v.block_id, height: staircase[y] }]);
+  }
+
+  // apply water fixes
+  // apply dirt fixes
+  // apply lava fixes?
+
+  return space;
+};
+
+const defaultNorthBorderBlock: MCBlockVariant = { block_id: 'stone', height_variant: 1 };
+
+export type MCBlock = {
+  block_id: string;
+  height: number;
+};
+
+export type MCBlockSpace = MCBlock[][][];
