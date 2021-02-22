d90da439bc0f246a955fc92626a52e72cd07ecf7
nicholaswrobinson <n21robin@gmail.com>
2021-02-23 01:16:46 +0200

    implemented bit array get method

--- generation/src/schema-generation/litematica/litematica-bit-array.ts
+++ generation/src/schema-generation/litematica/litematica-bit-array.ts
@@ -30,7 +30,7 @@ export type BitArray = {
 export const createBitArray = (volume: number, palette_length: number): BitArray => {
   const num_bits = getNeededBits(palette_length);
 
-  const array: long.Long[] = _.range(Math.ceil(volume * num_bits) / 64).map(() => [0, 0]);
+  const array: long.Long[] = _.range(Math.ceil(volume * num_bits)).map(() => [0, 0]);
   const mask = (1 << num_bits) - 1;
   return {
     volume,
@@ -74,15 +74,50 @@ export const set = (bit_array: BitArray, index: number, value: number) => {
  * Unimplemented
  */
 export const get = (bit_array: BitArray, index: number) => {
-  // const startOffset = index * this.bitsPerEntry;
-  // const startArrIndex = startOffset >> 6;
-  // const endArrIndex = ((index + 1) * (this.bitsPerEntry - 1)) >> 6;
-  // const startBitOffset = startOffset & 0x3f;
-  //
-  // if (startArrIndex === endArrIndex) {
-  //   return (this.arr[startArrIndex] >>> startBitOffset) & this.maxEntryValue;
-  // } else {
-  //   const endOffset = 64 - startBitOffset;
-  //   return ((this.arr[startArrIndex] >>> startBitOffset) | (this.arr[endArrIndex] << endOffset)) & this.maxEntryValue;
-  // }
+  const startOffset = index * bit_array.num_bits;
+  const startArrIndex = startOffset >> 6;
+  const endArrIndex = ((index + 1) * (bit_array.num_bits - 1)) >> 6;
+  const startBitOffset = startOffset & 0x3f;
+
+  if (startArrIndex === endArrIndex) {
+    return long.and(long.shiftRight(bit_array.array[startArrIndex], startBitOffset), [0, bit_array.mask]);
+  } else {
+    const endOffset = 64 - startBitOffset;
+
+    return long.and(
+      long.or(
+        long.shiftRight(bit_array.array[startArrIndex], startBitOffset),
+        long.shiftLeft(bit_array.array[endArrIndex], endOffset)
+      ),
+      [0, bit_array.mask]
+    );
+  }
+};
+
+/**
+ * Extract only the necessary longs from the internal storage array. This will effectively drop all trailing longs
+ * that are '0' or has had no operations performed against it
+ *
+ * [[0, 1234], [0, 4321], [0, 0], [0, 0], [0, 0] ...]
+ * ->
+ * [[0, 1234], [0, 0]]
+ */
+export const drain = (bit_array: BitArray) => {
+  const last_relevant_index = bit_array.array.reduce((last, long, i) => {
+    const [low, high] = long;
+    if (low === 0 && high === 0) {
+      if (last !== -1) {
+        return last;
+      } else {
+        return i;
+      }
+    }
+    return -1;
+  }, -1);
+
+  if (last_relevant_index === -1) {
+    return bit_array.array;
+  }
+
+  return bit_array.array.slice(0, last_relevant_index + 1);
 };
