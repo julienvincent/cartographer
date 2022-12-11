8cc8cbf6dd75baa7eb07bf00dc266e27a2e03827
Julien Vincent <m@julienvincent.io>
2022-12-11 22:10:04 +0100

    Describe block properties

--- pixels/src/defs/index.ts
+++ pixels/src/defs/index.ts
@@ -13,9 +13,22 @@ export type BlockAttributes = Record<string, string> & {
   flammable?: boolean;
   liquid?: boolean;
 };
+
 export type MCBlockDefinition = {
+  /**
+   * The minecraft block ID. This will be something like `minecraft:air`
+   */
   id: string;
+
+  /**
+   * Stores useful attributes about the block that can be used during the schema block space generation
+   */
   attributes?: BlockAttributes;
+
+  /**
+   * Stores properties about how the block should be placed. This could be things like `{axis: 'y'}` on a
+   * directional block such as `minecraft:birch_log` to indicate the direction the block is facing.
+   */
   properties?: Record<string, string>;
 };
 
