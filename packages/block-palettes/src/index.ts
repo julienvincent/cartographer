d63adfc2615faf45162c3ec47fe33a4c65ed3457
Julien Vincent <m@julienvincent.io>
2024-02-04 18:06:17 +0100

    Add color palette for 1.20

--- packages/block-palettes/src/index.ts
+++ packages/block-palettes/src/index.ts
@@ -1,9 +1,11 @@
 import { BlockPalette } from '@cartographer/pixels';
 
+import palette_1_20 from './palettes/1.20.json';
 import palette_1_19 from './palettes/1.19.json';
 import palette_1_18 from './palettes/1.18.json';
 
 export const palettes = {
+  '1.20': palette_1_20 as unknown as BlockPalette,
   '1.19': palette_1_19 as unknown as BlockPalette,
   '1.18': palette_1_18 as unknown as BlockPalette
 };
